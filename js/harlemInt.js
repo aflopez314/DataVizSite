30765826626003337126232db187784ceaf9898d
aflopez314 <aflopez314@gmail.com>
2018-02-23 17:09:08 -0500

    20th commit

--- js/harlemInt.js
+++ js/harlemInt.js
@@ -15,32 +15,15 @@ function preload(){
 
 function setup() {
     var cnv = createCanvas(640, 480);
-    cnv.parent('myCanvas2');
+    cnv.parent('myCanvas3');
     colorMode(HSB);
     background(53, 4, 11);
     textFont('Roboto Condensed');
     textAlign(BOTTOM);
+    fill(35, 50, 50);
     textSize(14);
+    text("Year of Housing Units by Owner Type, 1800 to 2015 (L to R)", 160, 440);
     fill(35, 60, 60);
-    text('Year of Housing Units by Owner Type, 1800 to 2015 (L to R)', 160, 440);
-    
-     /* start line */
-    fill(255);
-    line(startX - 3, startY + 50, startX - 3, startY + 362);
-    textSize(12);
-    text('1800', startX - 8, startY + 390);
-    
-       /* end line */
-    fill(255);
-    line(endX + 10, startY + 50, endX + 10, startY + 362);
-    textSize(12);
-    text('2015', endX + 8, startY + 390);
-    
-     /* mid line */
-    fill(255);
-    line(startX + 191, startY + 50, startX + 191, startY + 362);
-    textSize(12);
-    text('1910', startX + 188, startY + 390);
     
     
     /*print(harlemTable.getRowCount());
@@ -50,34 +33,34 @@ function setup() {
     
 }
 
-function draw(){
+function drawDots(){
     colorMode(HSB);
     fill(255);
-    
     for (var i = 0; i < harlemTable.getRowCount(); i++){
         var year = harlemTable.getNum(i, 'YearBuilt');
         var own = harlemTable.getString(i, 'OwnerType');
         
-        if (own == 'X'){
+        if (own == 'X' && year == 1910){
             positionX = map(year, startYear, endYear, startX, endX);
             positionY = map(10, 0, 100, startY, endY);
             ellipse(positionX, positionY, 6, 6);
-            textAlign(LEFT);
             textSize(25);
+            textAlign(LEFT);
             text(own, 475, 75);
             
         } else if (own == 'P'){
             positionX = map(year, startYear, endYear, startX, endX);
             positionY = map(20, 0, 100, startY, endY);
             ellipse(positionX, positionY, 6, 6);
-            textAlign(LEFT);
             textSize(25);
+            textAlign(LEFT);
             text(own, 475, 135);
             
         } else if (own == 'C'){
             positionX = map(year, startYear, endYear, startX, endX);
             positionY = map(30, 0, 100, startY, endY);
             ellipse(positionX, positionY, 6, 6);
+            textSize(25);
             textAlign(LEFT);
             text(own, 475, 195);
             
@@ -85,28 +68,53 @@ function draw(){
             positionX = map(year, startYear, endYear, startX, endX);
             positionY = map(40, 0, 100, startY, endY);
             ellipse(positionX, positionY, 6, 6);
-            textAlign(LEFT);
             textSize(25);
+            textAlign(LEFT);
             text(own, 475, 245);
             
         } else if (own == 'M'){
             positionX = map(year, startYear, endYear, startX, endX);
             positionY = map(50, 0, 100, startY, endY);
             ellipse(positionX, positionY, 6, 6);
-            textAlign(LEFT);
             textSize(25);
+            textAlign(LEFT);
             text(own, 475, 305);
             
         } else {
             positionX = map(year, startYear, endYear, startX, endX);
             positionY = map(60, 0, 100, startY, endY);
             ellipse(positionX, positionY, 6, 6);
-            textAlign(LEFT);
             textSize(25);
+            textAlign(LEFT);
             text('Null', 475, 365);
             
-        }
-        
-    }
-   
+        }  
+    }   
+}
+
+function drawLines(){
+    /* start line */
+    fill(255);
+    line(startX - 3, startY + 50, startX - 3, startY + 362);
+    textSize(12);
+    text('1800', startX - 8, startY + 390);
+    
+    /* end line */
+    fill(255);
+    line(endX + 10, startY + 50, endX + 10, startY + 362);
+    textSize(12);
+    text('2015', endX + 8, startY + 390);
+    
+    /* mid line */
+    fill(255);
+    line(startX + 191, startY + 50, startX + 191, startY + 362);
+    textSize(12);
+    text('1910', startX + 188, startY + 390);
+    
+}
+
+function draw(){
+    drawDots();
+    drawLines();
+    parent.document('myCanvas3');
 }
