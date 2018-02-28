b392b6934df2804d33ef566117bf1eb9f6ae8bc8
aflopez314 <aflopez314@gmail.com>
2018-02-28 03:14:07 -0500

    21 commit

--- js/harlemInt.js
+++ js/harlemInt.js
@@ -12,13 +12,13 @@ var buttonLength = 90;
 var buttonHeight = 15;
 var buttonSpacing = 10;
 var selectedButton = 0;
-var oldUnits = new p5.Table;
-var midUnits = new p5.Table;
-var newUnits = new p5.Table;
+var oldUnits = new p5.Table();
+var midUnits = new p5.Table();
+var newUnits = new p5.Table();
 
 
 function preload(){
-    harlemTable = loadTable("data/harlem2.csv", 'csv', 'header');
+    harlemTable = loadTable('data/harlem2.csv', 'csv', 'header');
     console.log('table has loaded...');
     
 }
@@ -26,32 +26,7 @@ function preload(){
 function setup() {
     var cnv = createCanvas(640, 480);
     cnv.parent('myCanvas3');
-    colorMode(HSB);
-    background(53, 4, 11);
     textFont('Roboto Condensed');
-    textAlign(BOTTOM);
-    fill(35, 50, 50);
-    textSize(14);
-    text("Year of Housing Units by Owner Type, 1800 to 2015 (L to R)", 160, 440);
-    fill(35, 60, 60);
-    
-      /* start line */
-    fill(255);
-    line(startX - 3, startY + 50, startX - 3, startY + 362);
-    textSize(12);
-    text('1800', startX - 8, startY + 390);
-    
-    /* end line */
-    fill(255);
-    line(endX + 10, startY + 50, endX + 10, startY + 362);
-    textSize(12);
-    text('2015', endX + 8, startY + 390);
-    
-    /* mid line */
-    fill(255);
-    line(startX + 191, startY + 50, startX + 191, startY + 362);
-    textSize(12);
-    text('1910', startX + 188, startY + 390);
     
     
     /*print(harlemTable.getRowCount());
@@ -62,6 +37,31 @@ function setup() {
     
 }
 
+function drawLabels(){
+    colorMode(HSB);
+    fill(35, 60, 60);
+    textSize(14);
+    text("Year of Housing Units by Owner Type, 1800 to 2015 (L to R)", 170, 440);
+    
+     /* start line */
+    fill(0,0,100);
+    line(startX - 3, startY + 50, startX - 3, startY + 362);
+    textSize(12);
+    text('1800', startX - 8, startY + 390);
+    
+    /* end line */
+    line(endX + 10, startY + 50, endX + 10, startY + 362);
+    textSize(12);
+    text('2015', endX + 8, startY + 390);
+    fill(0,0,100);
+    
+    /* mid line */
+    line(startX + 191, startY + 50, startX + 191, startY + 362);
+    textSize(12);
+    text('1910', startX + 188, startY + 390);
+    fill(0,0,100);
+}
+
 function createNewTable(){
   oldUnits.addColumn('YearBuilt');
   oldUnits.addColumn('OwnerType');
@@ -70,28 +70,32 @@ function createNewTable(){
   newUnits.addColumn('YearBuilt');
   newUnits.addColumn('OwnerType');
   for (var i = 0; i < harlemTable.getRowCount(); i++) {
-    var age = harlemTable.getNum(i, 'age');
+    var age = harlemTable.getNum(i, 'YearBuilt');
     if (age <= 1930) {
       var newRow = oldUnits.addRow();
-      newRow.setString('YearBuilt', harlemTable.getString(i, 'YearBuilt'));
-      newRow.setNum('OwnerType', harlemTable.getNum(i, 'OwnerType'));
-    }
-    else {
-        age > 1930 && age <= 1961;
-      var newRow = harlemBudget.addRow();
-      newRow.setString('O', harlemTable.getString(i, 'YearBuilt'));
-      newRow.setNum('OwnerType', moviesTable.getNum(i, 'OwnerType'));
+      newRow.setString('OwnerType', harlemTable.getString(i, 'OwnerType'));
+      newRow.setNum('YearBuilt', harlemTable.getNum(i, 'YearBuilt'));
+    } else if (age > 1930 && age <= 1961) {
+      var newRow = midUnits.addRow();
+      newRow.setString('OwnerType', harlemTable.getString(i, 'OwnerType'));
+      newRow.setNum('YearBuilt', harlemTable.getNum(i, 'YearBuilt'));
+    } else {
+      var newRow = newUnits.addRow();
+      newRow.setString('OwnerType', harlemTable.getString(i, 'OwnerType'));
+      newRow.setNum('YearBuilt', harlemTable.getNum(i, 'YearBuilt'));
     }
   }
   print('New tables created...');
 }
 
-function drawDots(){
-    colorMode(HSB);
-    fill(255);
-    for (var i = 0; i < harlemTable.getRowCount(); i++){
-        var year = harlemTable.getNum(i, 'YearBuilt');
-        var own = harlemTable.getString(i, 'OwnerType');
+
+function drawYears(){
+    if (selectedButton == 0){
+        fill(255);
+        noStroke();
+        for (var i = 0; i < harlemTable.getRowCount(); i++){
+            var year = harlemTable.getNum(i, 'YearBuilt');
+            var own = harlemTable.getString(i, 'OwnerType');
         
         if (own == 'X'){
             positionX = map(year, startYear, endYear, startX, endX);
@@ -142,24 +146,182 @@ function drawDots(){
             text('Null', 475, 365);
             
         }  
-    } 
-}
-
-
-function drawYears(){
-    if (selectedButton == 0){
-    }  
-    if (selectedButton == 1){
-        fill(145);
-        for (var i = 0; i < harlemTable.getRowCount(); i++){
-            var year = harlemTable.getNum(i, 'YearBuilt');
-            var own = harlemTable.getString(i, 'OwnerType');
-            own == 'X' && year <= 1930;
+    
+    }
+    } else if (selectedButton == 1){
+        colorMode(HSB);
+        fill(35, 50, 50);
+        noStroke();
+        for (var i = 0; i < oldUnits.getRowCount(); i++){
+            var year = oldUnits.getNum(i, 'YearBuilt');
+            var own = oldUnits.getString(i, 'OwnerType');
+        if (own == 'X'){
             positionX = map(year, startYear, endYear, startX, endX);
             positionY = map(10, 0, 100, startY, endY);
             ellipse(positionX, positionY, 6, 6);
+            textSize(25);
             textAlign(LEFT);
             text(own, 475, 75);
+            
+        } else if (own == 'P'){
+            positionX = map(year, startYear, endYear, startX, endX);
+            positionY = map(20, 0, 100, startY, endY);
+            ellipse(positionX, positionY, 6, 6);
+            textSize(25);
+            textAlign(LEFT);
+            text(own, 475, 135);
+            
+        } else if (own == 'C'){
+            positionX = map(year, startYear, endYear, startX, endX);
+            positionY = map(30, 0, 100, startY, endY);
+            ellipse(positionX, positionY, 6, 6);
+            textSize(25);
+            textAlign(LEFT);
+            text(own, 475, 195);
+            
+        } else if (own == 'O'){
+            positionX = map(year, startYear, endYear, startX, endX);
+            positionY = map(40, 0, 100, startY, endY);
+            ellipse(positionX, positionY, 6, 6);
+            textSize(25);
+            textAlign(LEFT);
+            text(own, 475, 245);
+            
+        } else if (own == 'M'){
+            positionX = map(year, startYear, endYear, startX, endX);
+            positionY = map(50, 0, 100, startY, endY);
+            ellipse(positionX, positionY, 6, 6);
+            textSize(25);
+            textAlign(LEFT);
+            text(own, 475, 305);
+            
+        } else {
+            positionX = map(year, startYear, endYear, startX, endX);
+            positionY = map(60, 0, 100, startY, endY);
+            ellipse(positionX, positionY, 6, 6);
+            textSize(25);
+            textAlign(LEFT);
+            text('Null', 475, 365);
+            
+        }
+            
+    }
+    } else if (selectedButton == 2){
+        colorMode(HSB);
+        fill(35, 50, 50);
+        noStroke();
+        for (var i = 0; i < midUnits.getRowCount(); i++){
+            var year = midUnits.getNum(i, 'YearBuilt');
+            var own = midUnits.getString(i, 'OwnerType');
+            
+        if (own == 'X'){
+            positionX = map(year, startYear, endYear, startX, endX);
+            positionY = map(10, 0, 100, startY, endY);
+            ellipse(positionX, positionY, 6, 6);
+            textSize(25);
+            textAlign(LEFT);
+            text(own, 475, 75);
+            
+        } else if (own == 'P'){
+            positionX = map(year, startYear, endYear, startX, endX);
+            positionY = map(20, 0, 100, startY, endY);
+            ellipse(positionX, positionY, 6, 6);
+            textSize(25);
+            textAlign(LEFT);
+            text(own, 475, 135);
+            
+        } else if (own == 'C'){
+            positionX = map(year, startYear, endYear, startX, endX);
+            positionY = map(30, 0, 100, startY, endY);
+            ellipse(positionX, positionY, 6, 6);
+            textSize(25);
+            textAlign(LEFT);
+            text(own, 475, 195);
+            
+        } else if (own == 'O'){
+            positionX = map(year, startYear, endYear, startX, endX);
+            positionY = map(40, 0, 100, startY, endY);
+            ellipse(positionX, positionY, 6, 6);
+            textSize(25);
+            textAlign(LEFT);
+            text(own, 475, 245);
+            
+        } else if (own == 'M'){
+            positionX = map(year, startYear, endYear, startX, endX);
+            positionY = map(50, 0, 100, startY, endY);
+            ellipse(positionX, positionY, 6, 6);
+            textSize(25);
+            textAlign(LEFT);
+            text(own, 475, 305);
+            
+        } else {
+            positionX = map(year, startYear, endYear, startX, endX);
+            positionY = map(60, 0, 100, startY, endY);
+            ellipse(positionX, positionY, 6, 6);
+            textSize(25);
+            textAlign(LEFT);
+            text('Null', 475, 365);
+            
+        }
+        
+    }
+    } else{
+        colorMode(HSB);
+        fill(35, 50, 50);
+        noStroke();
+        for (var i = 0; i < newUnits.getRowCount(); i++){
+            var year = newUnits.getNum(i, 'YearBuilt');
+            var own = newUnits.getString(i, 'OwnerType');
+            
+        if (own == 'X'){
+            positionX = map(year, startYear, endYear, startX, endX);
+            positionY = map(10, 0, 100, startY, endY);
+            ellipse(positionX, positionY, 6, 6);
+            textSize(25);
+            textAlign(LEFT);
+            text(own, 475, 75);
+            
+        } else if (own == 'P'){
+            positionX = map(year, startYear, endYear, startX, endX);
+            positionY = map(20, 0, 100, startY, endY);
+            ellipse(positionX, positionY, 6, 6);
+            textSize(25);
+            textAlign(LEFT);
+            text(own, 475, 135);
+            
+        } else if (own == 'C'){
+            positionX = map(year, startYear, endYear, startX, endX);
+            positionY = map(30, 0, 100, startY, endY);
+            ellipse(positionX, positionY, 6, 6);
+            textSize(25);
+            textAlign(LEFT);
+            text(own, 475, 195);
+            
+        } else if (own == 'O'){
+            positionX = map(year, startYear, endYear, startX, endX);
+            positionY = map(40, 0, 100, startY, endY);
+            ellipse(positionX, positionY, 6, 6);
+            textSize(25);
+            textAlign(LEFT);
+            text(own, 475, 245);
+            
+        } else if (own == 'M'){
+            positionX = map(year, startYear, endYear, startX, endX);
+            positionY = map(50, 0, 100, startY, endY);
+            ellipse(positionX, positionY, 6, 6);
+            textSize(25);
+            textAlign(LEFT);
+            text(own, 475, 305);
+            
+        } else {
+            positionX = map(year, startYear, endYear, startX, endX);
+            positionY = map(60, 0, 100, startY, endY);
+            ellipse(positionX, positionY, 6, 6);
+            textSize(25);
+            textAlign(LEFT);
+            text('Null', 475, 365);
+            
+        }
     }
     }
 }
@@ -173,19 +335,29 @@ function drawButtons(){
         } else{
             fill(35, 30, 30);
         }
-        stroke(100);
+        stroke(0,0,100);
         rect(buttonStartX + i * (buttonLength + buttonSpacing), buttonStartY, buttonLength, buttonHeight);
-        fill(255);
+        fill(0,0,100);
         noStroke();
+        textSize(12);
         text(buttonLabels[i], buttonStartX + i *(buttonLength + buttonSpacing) + buttonLength/2, buttonStartY + 2);
     }
-    noLoop();
+}
+
+
+function draw(){
+    colorMode(HSB);
+    background(53, 4, 11);
+    drawLabels();
+    drawYears();
+    drawButtons();
 }
 
 function mousePressed(){
      if (mouseX >= buttonStartX && mouseX <= (buttonStartX + buttonLength) && mouseY >= buttonStartY && mouseY <= (buttonStartY + buttonHeight)) {
          selectedButton = 0;
-        /* redraw();*/
+         redraw();
+         
      } else if (mouseX >= (buttonStartX + buttonLength + buttonSpacing) && mouseX <= (buttonStartX + buttonLength * 2 + buttonSpacing) && mouseY >= buttonStartY && mouseY <= (buttonStartY + buttonHeight)){
          selectedButton = 1;
          redraw();
@@ -199,11 +371,4 @@ function mousePressed(){
          redraw();
          
      }
-}
-
-function draw(){
-    /*drawLines();*/
-    drawButtons();
-    drawDots();
-    parent.document('myCanvas3');
 }
