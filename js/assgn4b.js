fa0d51b7613975b1b4650857b237c0758812cf80
aflopez314 <35818794+aflopez314@users.noreply.github.com>
2019-03-05 11:36:29 -0600

    Update assgn4b.js

--- js/assgn4b.js
+++ js/assgn4b.js
@@ -1,46 +1,35 @@
-console.log("is this working??");
-
 function setup(){
     createCanvas(500,500);
 }
 
 function draw(){
     
-        for (var i = 0; i < 22.6; i++){
-        for (var j = 0; j < 22.6; j++){
+        for (var i = 0; i < 20; i++){ 
+        for (var j = 0; j < 20; j++){
             
             var x1 = i;
             var y1 = j;
-            var x2 = mouseX;
-            var y2 = mouseY;
-            var d = int(dist(x1,y1,x2,y2));
-            var r = .5 - d;
-            if ( r > -30 || r < 30){
-            fill(105,105,105);
-            ellipse(width/30 + 30*i, height/30 + 30*j, 30-d, 30-d);
-            }
-            else{
-            fill(105,105,105);
-            ellipse(width/30 + 30*i, height/30 + 30*j, 30, 30);
-            }
-        }
+						var xpos = width/10 + 20*i;
+						var ypos = height/10 + 20*j;
+						var d = dist(xpos, ypos, mouseX, mouseY);
+						if (d < 650){
+							if (xpos < mouseX < xpos || ypos < mouseY < ypos){
+										fill(25,105,105);
+										ellipse(xpos, ypos, d/20, d/20);
+								}
+						}
+						else{
+							fill(25,105,105);
+							ellipse(xpos, ypos, 20, 20)
+						}
+						
         }
     }
-/*
-function draw(){
-     background(232, 44, 12);
-    if (mouseIsPressed){
-        fill(255,0, 0, 5);
-    }
-    else{
-        fill(255);
-    }
-    for (var i = 0; i < 10; i++){
-        for (var j = 0; j < 10; j++){
-        ellipse(mouseX + 30*i, mouseY + 30*j, 20, 20);   
-        }
-    }   
 }
-*/
 
-
+function mouseMoved(){
+	clear();
+// 	text(mouseX, 450,300);
+// 	text(20-mouseX/4, 450,350);
+	redraw();
+}
