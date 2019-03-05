7b3be8453aaef533aef993b8af64d4648f4a0ae6
aflopez314 <35818794+aflopez314@users.noreply.github.com>
2019-03-05 11:40:00 -0600

    Update assgn4b.js
    Fixed the dist() formula as well as added the constraints to the allowed distance.

--- js/assgn4b.js
+++ js/assgn4b.js
@@ -7,21 +7,22 @@ function draw(){
         for (var i = 0; i < 20; i++){ 
         for (var j = 0; j < 20; j++){
             
-            var x1 = i;
-            var y1 = j;
-						var xpos = width/10 + 20*i;
-						var ypos = height/10 + 20*j;
-						var d = dist(xpos, ypos, mouseX, mouseY);
-						if (d < 650){
-							if (xpos < mouseX < xpos || ypos < mouseY < ypos){
-										fill(25,105,105);
-										ellipse(xpos, ypos, d/20, d/20);
-								}
-						}
-						else{
-							fill(25,105,105);
-							ellipse(xpos, ypos, 20, 20)
-						}
+	var x1 = i;
+	var y1 = j;
+	var xpos = width/10 + 20*i;
+	var ypos = height/10 + 20*j;
+	var d = dist(xpos, ypos, mouseX, mouseY);
+		
+	if (d < 650){
+		if (xpos < mouseX < xpos || ypos < mouseY < ypos){
+			fill(25,105,105);
+			ellipse(xpos, ypos, d/20, d/20);
+		}
+	}
+	else{
+		fill(25,105,105);
+		ellipse(xpos, ypos, 20, 20)
+	}
 						
         }
     }
